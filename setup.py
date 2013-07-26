00a96d0c434d0f7e835cd71504c80774ff8858ef
Trey Stout <trey@keep.com>
2013-07-26 14:44:35 +0000

    upped version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pypb',
-    version='1.0.0',
+    version='1.0.1',
     author='Trey Stout',
     author_email='treystout@gmail.com',
     packages=['pypb', 'pypb.test'],
